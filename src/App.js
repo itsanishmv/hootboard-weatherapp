609d807e12bab893bf4b20f865f84caef20b324f
Anish M V <anishmv@kofukuidealabs.com>
2024-03-12 19:03:32 +0530

    setup basic routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Form from "./components/Form/Form";
+import WeatherInfo from "./components/WeatherInfo/WeatherInfo";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route element={<Form />} path="/" />
+        <Route element={<WeatherInfo />} path="/weather" />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Form/Form.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./Form.css";
+const Form = () => {
+  return <div>form component</div>;
+};
+
+export default Form;
