55640e22dc9f74d56ded66901e7e4006fc6e34a1
Anish M V <anishmv@kofukuidealabs.com>
2024-03-14 04:22:16 +0530

    added functionality to backarrow button

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,29 @@
 import React, { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import "./Header.css";
 import ArrowBack from "../../assets/arrowback.svg";
 function Header() {
   const location = useLocation();
+  const router = useNavigate();
   const [showIcon, setShowIcon] = useState(false);
   useEffect(() => {
     if (location.pathname !== "/") {
       setShowIcon(true);
+    } else {
+      setShowIcon(false);
     }
-  }, []);
+  }, [location.pathname]);
 
   return (
     <div className="header__container">
-      {showIcon && <img className="icon" src={ArrowBack} alt="arrow icon" />}
+      {showIcon && (
+        <img
+          onClick={() => router("/")}
+          className="icon"
+          src={ArrowBack}
+          alt="arrow icon"
+        />
+      )}
       <h1>Weather App</h1>
     </div>
   );
--- src/features/WeatherInfo/components/WeatherInfo/WeatherInfo.js
+++ src/features/WeatherInfo/components/WeatherInfo/WeatherInfo.js
@@ -21,7 +21,10 @@ function WeatherInfo() {
       <div className="weather__main__info">
         <img
           className="weather__cloud__icon"
-          src={`https://openweathermap.org/img/wn/${data?.weather[0]?.icon}@4x.png`}
+          src={
+            `https://openweathermap.org/img/wn/${data?.weather[0]?.icon}@4x.png` ||
+            Cloud
+          }
           alt="cloud"
         />
         <span className="weather__temperature">{data?.main.temp}°C</span>
