479b593bd529c187ec1e98147fba145eb7cc17e4
Anish M V <anishmv@kofukuidealabs.com>
2024-03-14 05:28:30 +0530

    error screen for city is not found

--- src/features/WeatherInfo/api/getWeatherInfo.js
+++ src/features/WeatherInfo/api/getWeatherInfo.js
@@ -1,7 +1,10 @@
 export async function getWeatherInfo(location) {
-  const api = await fetch(
+  const response = await fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${process.env.REACT_APP_APIKEY}`
   );
-  const response = await api.json();
-  return response;
+  if (!response.ok) {
+    throw new Error("City not found");
+  }
+
+  return await response?.json();
 }
--- src/features/WeatherInfo/components/WeatherInfo/WeatherInfo.js
+++ src/features/WeatherInfo/components/WeatherInfo/WeatherInfo.js
@@ -8,47 +8,57 @@ import Location from "../../../../assets/location.svg";
 
 function WeatherInfo() {
   const location = useLocation();
-  const [data, setData] = useState();
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(false);
   useEffect(() => {
-    const urlDissect = location.pathname.split("/");
-    const locationName = urlDissect[urlDissect.length - 1];
-    if (urlDissect.length > 2) {
-      getWeatherInfo(locationName).then((data) => setData(data));
+    const urlDissect = location?.pathname?.split("/");
+    const locationName = urlDissect[urlDissect?.length - 1];
+
+    if (urlDissect?.length > 2) {
+      getWeatherInfo(locationName)
+        .then((data) => setData(data))
+        .catch((error) => setError(error.message));
     }
-  }, [location.pathname]);
+  }, [location?.pathname]);
   return (
-    <div className="weather__container">
-      <div className="weather__main__info">
-        <img
-          className="weather__cloud__icon"
-          src={
-            `https://openweathermap.org/img/wn/${data?.weather[0]?.icon}@4x.png` ||
-            Cloud
-          }
-          alt="cloud"
-        />
-        <span className="weather__temperature">{data?.main.temp}°C</span>
-        <span className="weather__description">
-          {data?.weather[0]?.description}
-        </span>
-        <span className="weather__location">
-          <img src={Location} alt="location" />
-          {data?.name}, {data?.sys?.country}
-        </span>
-      </div>
-      <div className="weather__metainfo">
-        <WeatherMetainfo
-          data={data?.main?.feels_like}
-          label="Feels like"
-          symbol="°C"
-        />
-        <WeatherMetainfo
-          data={data?.main?.humidity}
-          label="Humidity"
-          symbol="%"
-        />
-      </div>
-    </div>
+    <>
+      {error ? (
+        <div className="city__not__found">
+          <img src={Cloud} alt="" />
+          {error}
+        </div>
+      ) : (
+        <div className="weather__container">
+          <div className="weather__main__info">
+            <img
+              className="weather__cloud__icon"
+              src={`https://openweathermap.org/img/wn/${data?.weather[0]?.icon}@4x.png`}
+              alt="cloud"
+            />
+            <span className="weather__temperature">{data?.main?.temp}°C</span>
+            <span className="weather__description">
+              {data?.weather[0]?.description}
+            </span>
+            <span className="weather__location">
+              <img src={Location} alt="location" />
+              {data?.name}, {data?.sys?.country}
+            </span>
+          </div>
+          <div className="weather__metainfo">
+            <WeatherMetainfo
+              data={data?.main?.feels_like}
+              label="Feels like"
+              symbol="°C"
+            />
+            <WeatherMetainfo
+              data={data?.main?.humidity}
+              label="Humidity"
+              symbol="%"
+            />
+          </div>
+        </div>
+      )}{" "}
+    </>
   );
 }
 
